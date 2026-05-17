f9278010a18750bec90277a527c8d8ac72b2f516
The King wilker <sandovalwilkerson700@gmail.com>
2026-05-16 22:54:57 -0400

    Actualizar daily.js

--- cmds/economy/daily.js
+++ cmds/economy/daily.js
@@ -1,5 +1,5 @@
 export default {
-  command: ['daily', 'diario'],
+  command: ['daily'],
   category: 'rpg',
   run: async (client, m, args, usedPrefix) => {
     const chat = global.db.data.chats[m.chat]
