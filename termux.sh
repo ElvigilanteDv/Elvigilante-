97e222ad782182660901cb51b2d27eb40e8dd50e
The King wilker <sandovalwilkerson700@gmail.com>
2026-05-21 14:34:23 -0400

    Actualizar termux.sh

--- termux.sh
+++ termux.sh
@@ -2,7 +2,7 @@
 
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="Miku-Wabot"  
-BOT_REPO="https://github.com/Dani-land/$BOT_DIR"
+BOT_REPO="https://github.com/dvwilker/$BOT_DIR"
 DB_FILE="database.json"
 
 GREEN='\033[32m'
